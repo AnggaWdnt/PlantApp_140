9656224a2c5508b48af0f61c984be9ed3eec713b
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-05-23 01:32:51 +0700

    tambah code untuk gambar feature

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -15,20 +15,55 @@ class Body extends StatelessWidget {
       child: Column(
         children: <Widget>[
           HeaderWithSearchBox(size: size),
-          TitleWithMoreBtn(
-            title: "Recommended",
-            press: () {},
-          ),
-          RecomendsPlants(),
-          TitleWithMoreBtn(
-            title: "Featured Plants",
-            press: (){}),
-            Container(width: size.width * 0.8,
-            height: 185,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10),
+          TitleWithMoreBtn(title: "Recommended",press: () {},),
+          RecomendsPlants(),TitleWithMoreBtn(title: "Featured Plants",press: (){}),
+            Row(
+              children: [
+                FeaturePlantCard(image: "assets\images\mawar.jpg", 
+                press: () {},
+                ),
+                FeaturePlantCard(image: "assets\images\mawar.jpg", 
+                press: () {},
+                ),
+                FeaturePlantCard(image: "assets\images\mawar.jpg", 
+                press: () {},
+                ),
+              ],
             ),
-            )
         ],
+      ),
+    );
+  }
+}
+
+class FeaturePlantCard extends StatelessWidget {
+  const FeaturePlantCard({
+    super.key, required this.image, required this.press,
+  });
+  final String image;
+  final Function press;
+
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+      margin: EdgeInsets.only(
+        left: kDefaultPadding, 
+        top: kDefaultPadding / 2, 
+        bottom: kDefaultPadding / 2,
+        ),
+      width: size.width * 0.8,
+      height: 185,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        image: DecorationImage(
+          fit: BoxFit.cover,
+        image: AssetImage(image),
+      ),
+      ),
       ),
     );
   }
