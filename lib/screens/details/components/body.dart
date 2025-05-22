90e464f3983f16a7fd9cc78047b4c68185be8cbf
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-05-23 03:02:36 +0700

    tambah code 4 icon

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -28,29 +28,10 @@ class Body extends StatelessWidget {
                        ),
                     ),
                     Spacer(),
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: size.height * 0.03),
-                      padding: EdgeInsets.all(kDefaultPadding / 2),
-                      height: 62, 
-                      width: 62,
-                      decoration: BoxDecoration(
-                        color: kBackgorundColor,
-                        borderRadius: BorderRadius.circular(6),
-                        boxShadow: [
-                          BoxShadow(
-                            offset: Offset(0, 15),
-                            blurRadius: 22,
-                            color: kPrimaryColor.withOpacity(0.22),
-                        ),
-                          BoxShadow(
-                            offset: Offset(-15, -15),
-                            blurRadius: 20,
-                            color: Colors.white
-                        ),
-                        ],
-                      ),
-                      child: SvgPicture.asset("assets\icons\matahari.svg"),
-                      )
+                    IconCard(icon: "assets\icons\matahari.svg"),
+                    IconCard(icon: "assets\icons\humidity.svg"),
+                    IconCard(icon: "assets\icons\water.svg"),
+                    IconCard(icon: "assets\icons\wind.svg"),
                    ],
                   ),
                 ),
@@ -81,5 +62,43 @@ class Body extends StatelessWidget {
         )
       ],
     );
+  }
+}
+
+class IconCard extends StatelessWidget {
+  const IconCard({
+    super.key, required this.icon,
+
+  });
+
+  final String icon;
+
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: size.height * 0.03),
+      padding: EdgeInsets.all(kDefaultPadding / 2),
+      height: 62, 
+      width: 62,
+      decoration: BoxDecoration(
+        color: kBackgorundColor,
+        borderRadius: BorderRadius.circular(6),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, 15),
+            blurRadius: 22,
+            color: kPrimaryColor.withOpacity(0.22),
+        ),
+          BoxShadow(
+            offset: Offset(-15, -15),
+            blurRadius: 20,
+            color: Colors.white
+        ),
+        ],
+      ),
+      child: SvgPicture.asset(icon),
+      );
   }
 }
