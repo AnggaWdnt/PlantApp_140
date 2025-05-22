7935264c4fef39a13addb2f8644f9020952893b4
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-05-23 03:28:02 +0700

    menambah dan memanggil title_and_price

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -4,6 +4,7 @@ import 'package:plantapp_140/constants.dart';
 
 import 'icon_card.dart';
 import 'image_and_icon.dart';
+import 'title_and_price.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -13,46 +14,13 @@ class Body extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ImageAndIcon(size: size),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-            child: Row(
-              children: <Widget>[
-               RichText(
-                text: TextSpan(
-                  children: [
-                    TextSpan(
-                      text: "Cactus\n", 
-                      style: Theme.of(context)
-                      .textTheme
-                      .headlineSmall
-                      ?.copyWith(
-                        color: kTextColor,
-                        fontWeight: FontWeight.bold),
-                        ),
-                        TextSpan(
-                          text: "Amerika", 
-                          style: TextStyle(
-                            fontSize: 20, 
-                            color: kPrimaryColor,
-                            fontWeight: FontWeight.w300,
-                        ),
-                      ),
-                  ],
-                ),
-                ),
-                Spacer(),
-                Text("\Rp25000", 
-                style: Theme.of(context)
-                .textTheme
-                .headlineSmall
-                ?.copyWith(color: kPrimaryColor),
-                ),
-             ],
+          TitleAndPrice(
+            title: 'Cactus', 
+            country: 'Amerika', 
+            price: 25000,
             ),
-          )
         ],
       ),
     );
   }
 }
-
