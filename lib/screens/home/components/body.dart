2c641b065a83bddf741ce6b3263863acbbdabf66
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-05-22 23:24:23 +0700

    tambah code text recomended

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -10,7 +10,47 @@ class Body extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[
-          HeaderWithSearchBox(size: size)
+          HeaderWithSearchBox(size: size),
+          Row(
+            children: [
+              TitleWithCustomUnderline(text:"Recomended"),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TitleWithCustomUnderline extends StatelessWidget {
+  const TitleWithCustomUnderline({
+    super.key, required this.text,
+  });
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 24,
+      child: Stack(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
+            child: Text("Recomended",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: EdgeInsets.only(right: kDefaultPadding / 4),
+              height: 7, 
+              color: kPrimaryColor.withOpacity(0.2),
+            ),
+          )
         ],
       ),
     );
