fb41b425d7395de91e2ce0732adade8a7fba34aa
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-05-23 01:38:36 +0700

    tambah file dan isi code featured plants

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:plantapp_140/constants.dart';
 import 'package:plantapp_140/screens/home/components/header_with_searchboy.dart';
 
+import 'featured_plants.dart';
 import 'recomend_plants.dart';
 import 'title_with_more_bttn.dart';
 
@@ -17,56 +18,9 @@ class Body extends StatelessWidget {
           HeaderWithSearchBox(size: size),
           TitleWithMoreBtn(title: "Recommended",press: () {},),
           RecomendsPlants(),TitleWithMoreBtn(title: "Featured Plants",press: (){}),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  FeaturePlantCard(image: "assets\images\mawar.jpg", 
-                  press: () {},
-                  ),
-                  FeaturePlantCard(image: "assets\images\mawar.jpg", 
-                  press: () {},
-                  ),
-                  FeaturePlantCard(image: "assets\images\mawar.jpg", 
-                  press: () {},
-                  ),
-                ],
-              ),
-            ),
+            FeaturedPlants(),
+            SizedBox(height: kDefaultPadding,)
         ],
-      ),
-    );
-  }
-}
-
-class FeaturePlantCard extends StatelessWidget {
-  const FeaturePlantCard({
-    super.key, required this.image, required this.press,
-  });
-  final String image;
-  final Function press;
-
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return GestureDetector(
-      onTap: press,
-      child: Container(
-      margin: EdgeInsets.only(
-        left: kDefaultPadding, 
-        top: kDefaultPadding / 2, 
-        bottom: kDefaultPadding / 2,
-        ),
-      width: size.width * 0.8,
-      height: 185,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        image: DecorationImage(
-          fit: BoxFit.cover,
-        image: AssetImage(image),
-      ),
-      ),
       ),
     );
   }
