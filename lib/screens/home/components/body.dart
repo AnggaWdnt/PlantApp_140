95c0c7b8fd5dafd7fa77701e45ed4034a69808ec
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2025-05-22 23:06:45 +0700

    tambah code header

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:plantapp_140/constants.dart';
+import 'package:plantapp_140/screens/home/components/header_with_searchboy.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -10,92 +11,6 @@ class Body extends StatelessWidget {
       child: Column(
         children: <Widget>[
           HeaderWithSearchBox(size: size)
-        ],
-      ),
-    );
-  }
-}
-
-class HeaderWithSearchBox extends StatelessWidget {
-  const HeaderWithSearchBox({
-    super.key,
-    required this.size,
-  });
-
-  final Size size;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(bottom: kDefaultPadding * 2.5),
-      padding: EdgeInsets.only(
-        left: kDefaultPadding,
-        right: kDefaultPadding,
-        bottom: 36 + kDefaultPadding,
-      ),
-      height: size.height * 0.2,
-      child: Stack(
-        children: <Widget>[
-          Container(
-            height: size.height * 0.2 - 27,
-            decoration: BoxDecoration(
-              color: kPrimaryColor,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(36),
-                bottomRight: Radius.circular(36),
-              ),
-            ),
-            child: Row(
-              children: <Widget> [
-                Text('HI Iscopy!', style: Theme.of(context).textTheme.headline5.copyWith(
-                  color: Colors.white, FontWeight.bold),
-                ),
-                Spacer(),
-                Image.asset("assets\images\logoplant.jpg")
-                ],
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              height: 54,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius:BorderRadius.circular(20),
-                boxShadow: [BoxShadow(
-                  offset: Offset(0, 10),
-                  blurRadius: 50,
-                  color: kPrimaryColor.withOpacity(0.23),
-                  ),
-                ],
-              ),
-              child: Row(
-                children: <Widget> [
-                  Expanded(
-                    child: TextField(
-                      onChanged: (value) {},
-                      decoration: InputDecoration(
-                        hintText: "Search",
-                        hintStyle: TextStyle(
-                          color: kPrimaryColor.withOpacity(0.5),    
-                       ),
-                       enabledBorder: InputBorder.none,
-                       focusedBorder: InputBorder.none, 
-                      //  suffixIcon:
-                      // SvgPicture.asset("assets/icons/search.svg"),
-                      ),
-                    ),
-                  ),
-                  SvgPicture.asset("assets\icons\search.svg"),
-                ],
-              ),
-            ),
-            ),
         ],
       ),
     );
